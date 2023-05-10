2d7efc441a659d3f3177266ac374785865506862
sstteeeenn05 <sstteeeenn05@gmail.com>
2023-05-10 18:05:39 +0800

    init multiplay & move nowMoving to server side

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -10,93 +10,141 @@ const server=app.listen(PORT);
 
 /**
  * @typedef {Object} Room
+ * @prop {string} status
+ * @prop {string} nowMoving
  * @prop {WebScoket} p0
  * @prop {WebScoket} p1
  * @prop {pipe.ChildProcess} process
+ * @prop {Array} queue
  */
 
 let wss=new SocketServer({server});
-/** @type {Map<number,Room>} */
-let room=new Map();
+/** @type {Map<string,Room>} */
+let roomList=new Map();
 
+/** @type {pipe.ChildProcess} */
 let process;
-
-/**
- * @typedef {Object} CreateArgs
- * @prop {boolean} isSingle
- * @prop {string} ipaddr
- * @prop {string} nickname
- */
-
-/**
- * @typedef {Object} JoinArgs
- * @prop {number} roomId
- * @prop {string} ipaddr
- * @prop {string} nickname
- */
 
 /**
  * @typedef {Object} Package
  * @prop {string} header
- * @prop {CreateArgs|JoinArgs} args
+ * @prop {string} ipaddr
+ * @prop {string} nickname
+ * @prop {boolean} isSingle
+ * @prop {string} roomId
  */
 
 wss.on('connection',(ws)=>{
     console.log(`web socket connected! (${ws.protocol})`);
-    switch(ws.protocol){
-        case "protocol-create-game":
-            createGame(ws);
-            break;
-        case "protocol-input-command":
-            inputCommand(ws);
-            break;
+    if(ws.protocol!="protocol-chess-game") return ws.close();
+    ws.onmessage=(e)=>{
+        ws.onmessage=undefined;
+        let package=JSON.parse(e.data.toString());
+        switch(package.header){
+            case "create":
+                createGame(ws,package);
+                break;
+            case "join":
+                joinGame(ws,package);
+                break;
+        }
     }
 })
 
-function createGame(ws){
-    console.log(ws.url);
-    process=pipe.execFile("application\\chess.exe");
+function createGame(ws,package){
+    let room={p0:ws,nowMoving:"white",queue:new Array()}
+    initGame(ws,room.queue);
+    if(package.isSingle){
+        room.status="single";
+        room.p1=ws;
+        room.process=pipe.execFile("application\\chess.exe");
+    }else room.status="waiting";
+    roomList.set("123",room);
+    ws.send("123");
+    if(package.isSingle) startGame("123");
 }
 
-function inputCommand(ws){
-    let queue=new Array();
-
-    if(!process) ws.close();
-
-    ws.on('message',(data)=>{
-        console.log("receive:"+data);
-        queue.push(data.toString());
+function joinGame(ws,package){
+    if(!roomList.has(package.roomId)) return ws.close();
+    let room=roomList.get(package.roomId);
+    if(room.status!="waiting") return ws.close();
+    new Promise((resolve,reject)=>{
+        room.p0.onmessage=(e)=>{
+            let data=JSON.parse(e.data.toString());
+            console.log(`receive:${data.type} > ${data.value}`);
+            if(data.value=="Y") resolve();
+            else reject();
+        }
+        room.p0.send(package);
+    }).then(()=>{
+        room.status="full";
+        initGame(ws,room.queue);
+        room.p1=ws;
+        room.process=pipe.execFile("application\\chess.exe");
+        startGame(package.roomId);
     })
+}
+
+function initGame(ws,queue){
+    ws.onmessage=(e)=>{
+        let data=JSON.parse(e.data.toString());
+        console.log(`receive:${data.type} > ${data.value}`);
+        switch(data.type){
+            case "command":
+                console.log("push to queue");
+                queue.push(data.value);
+                break;
+        }
+    }
+}
+
+function startGame(roomId){
+    let room=roomList.get(roomId);
+    let queue=room.queue;
+    let process=room.process;
+
+    if(!process){
+        room.p0.close();
+        room.p1.close();
+    }
 
     process.stdout.on('data',(data)=>{
         console.log("ondata!");
         data=data.toString().replaceAll(' ','.').replaceAll(/[\u0000-\u001F\u007F-\u009F]/g,'').split(';');
-        if(data.length!=4) ws.close();
-        else{
-            let obj={
-                status:data[0],
-                canUndo:data[1]==='1',
-                canRedo:data[2]==='1',
-                value:data[3]
-            }
-            console.log(`send:${obj.value} > ${obj.status}`);
-            ws.send(JSON.stringify(obj));
+        if(data.length!=4){
+            room.p0.close();
+            room.p1.close();
+            return;
         }
+        let obj={
+            status:data[0],
+            canUndo:data[1]==='1',
+            canRedo:data[2]==='1',
+            value:data[3]
+        }
+        //TODO: tell ws2 it's now your turn and deal with win/lose/tie
+        let ws1=room.nowMoving=='white'?room.p0:room.p1;
+        room.nowMoving=obj.status.split('.')[0];
+        let ws2=room.nowMoving=='white'?room.p0:room.p1;
+        console.log(`send:${obj.value} > ${obj.status}`);
+        ws1.send(JSON.stringify(obj));
     })
 
     let queueTimer=setInterval(()=>{
         if(queue.length){
-            let commands=queue.shift();
+            let commands=queue.shift().toString();
             console.log("execute:",commands);
             commands.split(' ').forEach((command)=>process.stdin.write(command+'\n'));
             process.stdin.write("\n");
         }
     },20)
 
-    ws.on('close',()=>{
+    let onclose=()=>{
         console.log("onclose!");
         clearInterval(queueTimer);
-    })
+    }
+    room.p0.onclose=onclose;
+    room.p1.onclose=onclose;
 }
 
 console.log("http://localhost:"+PORT);
--- FrontEnd/www/api/Game.js
+++ FrontEnd/www/api/Game.js
@@ -1,35 +1,66 @@
 const WS_URL="ws://localhost:1234";
 
+/**
+ * @typedef {Object} Package
+ * @prop {string} header
+ * @prop {string} ipaddr
+ * @prop {string} nickname
+ * @prop {boolean} isSingle
+ * @prop {string} roomId
+ */
+
 export class Game{
     constructor(){
-        new WebSocket(WS_URL,"protocol-create-game");
-        this.ws=new WebSocket(WS_URL,"protocol-input-command");
+        let ws=new WebSocket(WS_URL,"protocol-chess-game");
+        let pckg={
+            header:"create",
+            ipaddr:"127.0.0.1",
+            nickname:"abc",
+            isSingle:true,
+        }
+        new Promise((resolve,reject)=>{
+            ws.onopen=()=>{
+                ws.onmessage=(e)=>{
+                    this.ws=ws;
+                    this.roomId=e.data.toString();
+                    resolve("Web Socket is ready!");
+                }
+                ws.onclose=()=>reject("Web Socket is closed!");
+                ws.onerror=(event)=>reject(event);
+                ws.send(JSON.stringify(pckg));
+            }
+        })
     }
-    generatePromise(command,valueLambda=(value)=>{return value}){
+    sendCommand(command,valueLambda=(value)=>{return value}){
         return new Promise((resolve,reject)=>{
+            if(!this.ws) reject("Web Socket is not ready!");
             this.ws.onmessage=(event)=>{
                 let data=JSON.parse(event.data.toString());
                 data.value=valueLambda(data.value);
                 resolve(data);
             }
+            this.ws.onclose=()=>reject("Web Socket is closed!");
             this.ws.onerror=(event)=>reject(event);
-            this.ws.send(command);
+            this.ws.send(JSON.stringify({
+                type:"command",
+                value:command
+            }));
         })
     }
     getBoard(){
-        return this.generatePromise(
+        return this.sendCommand(
             "print",
             (value)=>{return value.match(/.{1,8}/g)}
         )
     }
     promotion(choice){
-        return this.generatePromise(choice)
+        return this.sendCommand(choice)
     }
     move(x1,y1,x2,y2){
-        return this.generatePromise(`move ${x1} ${y1} ${x2} ${y2}`)
+        return this.sendCommand(`move ${x1} ${y1} ${x2} ${y2}`)
     }
     preview(x,y){
-        return this.generatePromise(
+        return this.sendCommand(
             `preview ${x} ${y}`,
             (value)=>{
                 return value.split("").map(char=>{return parseInt(char)})
